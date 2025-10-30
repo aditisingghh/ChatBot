15e7ef388eb6f5ad0e22ee236c06aa5411bcada5
Aditi Singh <aditiisinghh04@gmail.com>
2025-10-31 02:11:34 +0530

    root layout component

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "AssistAI Chatbot | Aditi Singh",
+  description: "An intelligent and friendly chatbot built with Next.js and Gemini API.",
+  icons: {
+    icon: "./favicon.png",
+  },
+};
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-100`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
